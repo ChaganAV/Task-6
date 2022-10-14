6b689b43bd573eb7803d04d7d053adb0e9c582c1
Alexandr <jwchaganav@gmail.com>
2022-10-14 16:21:50 +0400

    сделали задачу 6

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число: ");
+string? sNum = Console.ReadLine();
+
+int num = Convert.ToInt32(sNum);
+ 
+if (num%2 == 0)
+    Console.WriteLine("Число четное!");
+else
+    Console.WriteLine("Число нечетное!");
